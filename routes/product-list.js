638c6eb66d2a17cb565afe5516d917d757f10909
Manu <mruanodevelopment@gmail.com>
2018-04-22 11:37:36 +0100

    routes update

--- routes/product-list.js
+++ routes/product-list.js
@@ -3,6 +3,37 @@ var config = require('../config/config');
 // Mock API using fixture so we're not dependent on network connectivity
 var allProducts = require(config.ROOT +'/fixtures/products.json').data;
 
+const sort = (data, sortBy) => {
+
+    if(sortBy === 'low-high') {
+        return Object.values(data).sort((a, b) => {
+            return a.grossPrice - b.grossPrice
+        })
+    }
+
+    if(sortBy === 'high-low') {
+        return Object.values(data).sort((a, b) => {
+            return a.grossPrice - b.grossPrice
+        }).reverse()
+    }
+
+    if(sortBy === 'name') {
+        return Object.values(data).sort((a, b) => {
+            const nameA = a.name.toLowerCase()
+            const nameB = b.name.toLowerCase()
+            if (nameA < nameB)
+                return -1
+            if (nameA > nameB)
+                return 1
+            return 0
+        })
+    }
+
+    else {
+        return data
+    }
+
+}
 var routes = {
     init: function(app) {
 
@@ -10,26 +41,33 @@ var routes = {
             var total = allProducts.length;
             var offset = parseInt(req.query.offset) || 0;
             var limit = parseInt(req.query.limit) || 60;
+            var sortBy = req.query.sortBy || '';
             if (offset > total) {
                 return res.type('json').sendStatus(400);
             }
+
+            const data = allProducts.slice(offset, offset+limit).map(function(product) {
+
+                return {
+                    id: product.id,
+                    name: product.name.en,
+                    price: '£' + product.price.gross / product.price.divisor,
+                    grossPrice: product.price.gross / product.price.divisor,
+                    designer: product.brand.name.en,
+                    image: {
+                        outfit: '//cache.net-a-porter.com/images/products/'+product.id+'/'+product.id+'_ou_sl.jpg'
+                    }
+                }
+            })
 
             res.json({
                 offset: offset,
                 limit: limit,
                 total: total,
-                data: allProducts.slice(offset, offset+limit).map(function(product) {
-                    // Simplify payload - more data available in fixture
-                    return {
-                        id: product.id,
-                        name: product.name.en,
-                        price: '£' + product.price.gross / product.price.divisor,
-                        designer: product.brand.name.en,
-                        image: {
-                            outfit: '//cache.net-a-porter.com/images/products/'+product.id+'/'+product.id+'_ou_sl.jpg'
-                        }
-                    }
-                })
+                sortBy: sortBy,
+                data: sortBy.length > 1
+                    ? sort(data, sortBy)
+                    : data
             })
 
         })
